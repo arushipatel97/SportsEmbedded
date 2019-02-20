c9df42a976f324016dd26d7a862b1315ff050785
Arushi <arushipatel97@gmail.com>
2019-02-20 17:09:51 -0500

    better resolution

--- try5.py
+++ try5.py
@@ -4,7 +4,7 @@ import numpy as np
 original = cv2.imread('pro/pro1.jpg')
 height, width, depth = original.shape
 ratio = height/width
-nHeight = 500
+nHeight = 1000
 nWidth = nHeight/ratio
 img = cv2.resize(original,(int(nWidth),int(nHeight)))
 
@@ -35,19 +35,24 @@ if len(contours) != 0:
 res1 = cv2.bitwise_and(gray_crop_img,gray_crop_img,mask = mask)
 res2 = cv2.adaptiveThreshold(res1,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,3,-5)
-res = cv2.dilate(res2, None, iterations=1)
+res = cv2.dilate(res2, None, iterations=2)
 res = cv2.erode(res, None, iterations=1)
-# res = cv2.Canny(res2,50,150,apertureSize = 5)
 
 # res = res2
+res2 = cv2.blur(res2,(3,3))
 
-minLineLength = 30
-maxLineGap = 10
-lines = cv2.HoughLinesP(res2,1,np.pi/180,18,minLineLength,maxLineGap)
+minLineLength = 100000
+maxLineGap = 100
+lines = cv2.HoughLinesP(res,1,np.pi/180,50,minLineLength,maxLineGap)
 # lines = cv2.HoughLines(res,1,np.pi/180,80)
-for line in lines:
-    for x1,y1,x2,y2 in line:
-    	cv2.line(disp,(x1,y1),(x2,y2),(0,255,0),1)
+print(len(lines))
+# for line in lines:
+#     for x1,y1,x2,y2 in line:
+#     # x1,y1,x2,y2 = line[0]
+#     	cv2.line(disp,(x1,y1),(x2,y2),(255,0,0),2)
+a,b,c = lines.shape
+for i in range(a):
+    cv2.line(disp, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
 
 cv2.imshow('res',res)
 cv2.imshow('res1',res1)
@@ -56,6 +61,9 @@ cv2.imshow('original',img)
 # cv2.imshow('threshold',th3)
 cv2.imshow('hsv',mask)
 cv2.waitKey(0)
+cv2.imwrite("final.png", disp)
+cv2.imwrite("res2.png", res2)
+cv2.imwrite("res.png", res)
 cv2.destroyAllWindows()
 
 # g = cv2.cvtColor(green_image, cv2.COLOR_BGR2GRAY)
